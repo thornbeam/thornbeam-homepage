2a1ed6a010d715caacb6bc34c54482b5a8f28544
thornbeam <thornbeam@posteo.eu>
2023-01-10 20:48:10 +0100

    Add keenSliderV1.js

--- components/test/keenSliderV1.js
+++ components/test/keenSliderV1.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
+import { Image } from "@chakra-ui/react";
 import { useKeenSlider } from "keen-slider/react";
 import "keen-slider/keen-slider.min.css";
 import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
 
-// style of arrows tempolarity removed!
+import UnionSpiel from "../../components/image_urls/unionSpiel.js";
 
 export default () => {
   const [currentSlide, setCurrentSlide] = useState(0);
@@ -22,12 +23,9 @@ export default () => {
     <>
       <div className="navigation-wrapper">
         <div ref={sliderRef} className="keen-slider">
-          <div className="keen-slider__slide number-slide1">1</div>
-          <div className="keen-slider__slide number-slide2">2</div>
-          <div className="keen-slider__slide number-slide3">3</div>
-          <div className="keen-slider__slide number-slide4">4</div>
-          <div className="keen-slider__slide number-slide5">5</div>
-          <div className="keen-slider__slide number-slide6">6</div>
+          {UnionSpiel.map((src, i) => (
+            <Image className="keen-slider__slide" src={src}></Image>
+          ))}
         </div>
         {loaded && instanceRef.current && (
           <>
