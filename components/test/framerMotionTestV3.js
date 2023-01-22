781ccdf06248139a90bafac3b406fb312469ee59
thornbeam <thornbeam@posteo.eu>
2023-01-22 20:55:59 +0100

    Update .gitignore

--- components/test/framerMotionTestV3.js
+++
@@ -1,7 +0,0 @@
-import { motion } from "framer-motion";
-
-const FramerMotionTestV3 = ({ children }) => {
-  return <motion.div whileTap={{ scale: 0.9 }}>{children}</motion.div>;
-};
-
-export default FramerMotionTestV3;
